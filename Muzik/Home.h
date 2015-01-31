05a4bfe8bdc5336abe8a359100ac2bdbaa71167e
Andrew Codispoti <andrewcodispoti@v1s-odd-172-31-237-222.rn.uwaterloo.ca>
2015-01-31 15:14:26 -0500

    Added new ui for a table view cell.

---
+++ Muzik/Home.h
@@ -0,0 +1,13 @@
+//
+//  Home.h
+//  Muzik
+//
+//  Created by Andrew Codispoti on 2015-01-31.
+//  Copyright (c) 2015 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Home : UITableViewController
+
+@end
---
+++ Muzik/Home.m
@@ -0,0 +1,13 @@
+//
+//  Home.m
+//  Muzik
+//
+//  Created by Andrew Codispoti on 2015-01-31.
+//  Copyright (c) 2015 Andrew Codispoti. All rights reserved.
+//
+
+#import "Home.h"
+
+@implementation Home
+
+@end
